bd47ca22113d11b4573c0d81956b57a61ccae15e
harithlakmith <hlakmith@gmail.com>
2021-03-25 00:40:21 +0530

    1240

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -1,41 +1,22 @@
-//import logo from './../../logo.svg';
-//import './SignIn.css';
 import React, {Component} from 'react';
 import axios from 'axios';
-//import Pass_Info_Update from './component/pass_info_update/Pass_Info_Update';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Redirect, withRouter} from 'react-router-dom';
-import { browserHistory } from 'history'
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-
-import AuthService from "../services/auth.service";
-
-const required = value => {
-    if (!value) {
-      return (
-        <div className="alert alert-danger" role="alert">
-          This field is required!
-        </div>
-      );
-    }
-  };
+import AuthService from "./../services/auth.service";
 
 class SignIn extends Component{
 
     constructor(props) {
         super(props);
-        this.handleLogin = this.handleLogin.bind(this);
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-    
         this.state = {
           username: "",
           password: "",
           loading: false,
           message: ""
         };
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
       }
 
       onChangeUsername(e) {
@@ -51,16 +32,15 @@ class SignIn extends Component{
       }
     
       handleLogin(e) {
-        e.preventDefault();
-    
-        this.setState({
-          message: "",
-          loading: true
-        });
+            e.preventDefault();
+        
+            this.setState({
+              message: "",
+              loading: true
+            });
+          
+       // this.form.validateAll();
 
-        this.form.validateAll();
-
-        if (this.checkBtn.context._errors.length === 0) {
             AuthService.login(this.state.username, this.state.password).then(
               () => {
 
@@ -82,76 +62,69 @@ class SignIn extends Component{
                     error.response.data.message) ||
                   error.message ||
                   error.toString();
-      
+
                 this.setState({
                   loading: false,
                   message: resMessage
                 });
               }
             );
-          } else {
-            this.setState({
-              loading: false
-            });
-          }
+              
 
         
     }
 
   /* ---*/
  
-  /*---*/
+ 
 
 
 render(){
     
- /* if (localStorage.getItem('user')){
+ if (localStorage.getItem('user')){
     return <Redirect to={'/home'} />
-}*/
+}
     
-    return (
-      <Form onSubmit={this.handleLogin}
-            ref={c => {
-            this.form = c;
-     }}>
-
-      <div className="container p-1">
-        <div className="box">
+return (
+  <form onSubmit={this.handleLogin}>
+    
+    <div class="container pt-3 px-5 mt-5">
+      <div class="mt-5">
           <h1>
-            <u> Login</u>
+             <u>Login</u>
           </h1>
-               
-            <hr/>
-  
-          <div className="row">
-              <div className="col-md-6">
              
-   
-        
-          <div className="form-group">
-          <Input nChantype="text" className="form-control" name="username" 
-           onChange={this.onChangeUsername}
-           value={this.state.username} placeholder="Email" required="required"/>
-          </div>
-          <div className="form-group">
-        <Input type="password" className="form-control" 
-              name="password"
-              value={this.state.password}
-              onChange={this.onChangePassword}
-              placeholder="Password" required="required"/>
-          </div>   
+          <br></br>
+  
+       <div class="row">
+          <div class="col-lg-7 px-5">
+             
+            <div class="form-group">
+             <input type="text" class="form-control" name="username" 
+                 onChange={this.onChangeUsername}
+                 value={this.state.username} placeholder="Email" required="required"/>
+            </div>
+
+
+            <div class="form-group">
+             <input type="password" class="form-control" name="password"
+                value={this.state.password}
+                onChange={this.onChangePassword}
+                placeholder="Password" required="required"/>
+            </div>   
+         
           
-          <div className="form-group">
-              <button 
-                className="btn btn-primary btn-block"
+           <div class="form-group text-center">
+              <button type="submit"
+                class="btn btn-primary btn-lg"
                 disabled={this.state.loading}
               >
                 {this.state.loading && (
-                  <span className="spinner-border spinner-border-sm"></span>
+                  <span class="spinner-border spinner-border-sm"></span>
                 )}
-                <span>&nbsp;Login </span>
+                <span>Login</span>
               </button>
-            </div>
+           </div>
 
             {this.state.message && (
               <div className="form-group">
@@ -160,63 +133,22 @@ render(){
                 </div>
               </div>
             )}
-            <CheckButton
-              style={{ display: "none" }}
-              ref={c => {
-                this.checkBtn = c;
-              }}
-            />
+           
   
-     
-            </div>
-              
           </div>
+
+          <div class="col-lg-5 px-4">
+            <img src="images/login.jpeg" alt="login" class="ml-5"/>
+          </div>
+                   
+        </div>
   
+      </div>
   
-  </div>
-  
-  </div>
-   </Form>   
+    </div>
+  </form>   
     );
   }
   }
   
   export default withRouter(SignIn);
-
-  /* <div className="form-group">
-              <button type="submit" className="btn btn-primary btn-lg">Login</button>
-          </div>*/
-          
-/*  submitHandler = e => {
-    e.preventDefault()
-    
-    const data ={
-        Email: this.email,
-        Password: this.password
-    }
-  axios
-  .post("http://localhost:5000/api/Accounts/Login", data)
-  .then(response => {
-      
-      localStorage.setItem('user', response.data)
-      this.setState({
-          loggedIn: true
-      })
-      this.props.setUser(response.data)
-
-      *//*
-      if(response.status == 200){
-          alert("Loggin Succesful!")
-      }
-      *//*
-  })
-  .catch(error => {
-      
-      localStorage.setItem('error', error.data)
-      this.setState({
-          loggedIn: false
-      })
-      //this.setState({errorMessage: 'Check your email and password!(' +error.message+ ')'})
-      
-      
-  })}*/
--- src/component/Test_case/Test_case.js
+++ src/component/Test_case/Test_case.js
@@ -1,33 +1,22 @@
-import logo from './../../logo.svg';
-//import './SignIn.css';
 import React, {Component} from 'react';
 import axios from 'axios';
-//import Pass_Info_Update from './component/pass_info_update/Pass_Info_Update';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Redirect, withRouter} from 'react-router-dom';
-import { browserHistory } from 'history'
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-
 import AuthService from "../../services/auth.service";
-
 
 class SignIn extends Component{
 
     constructor(props) {
         super(props);
-        this.handleLogin = this.handleLogin.bind(this);
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-    
         this.state = {
           username: "",
           password: "",
           loading: false,
           message: ""
         };
-        
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
       }
 
       onChangeUsername(e) {
@@ -43,18 +32,15 @@ class SignIn extends Component{
       }
     
       handleLogin(e) {
-        e.preventDefault();
-    
-        this.setState({
-          message: "",
-          loading: true
-        });
-      
-    
+            e.preventDefault();
+        
+            this.setState({
+              message: "",
+              loading: true
+            });
+          
+       // this.form.validateAll();
 
-        this.form.validateAll();
-
-        if (this.checkBtn.context._errors.length === 0) {
             AuthService.login(this.state.username, this.state.password).then(
               () => {
 
@@ -63,8 +49,10 @@ class SignIn extends Component{
                     this.props.history.push("/home")
                   }else if(role=='Administrator'){
                     this.props.history.push("/admin-dash")
+                  }else if(role=='BusController'){
+                    this.props.history.push("/bus-dashboard")
                   };
-                
+                //  this.props.history.push("/home")
                 window.location.reload();
               },
               error => {
@@ -74,67 +62,59 @@ class SignIn extends Component{
                     error.response.data.message) ||
                   error.message ||
                   error.toString();
-      
+
                 this.setState({
                   loading: false,
                   message: resMessage
                 });
               }
             );
-          } else {
-            this.setState({
-              loading: false
-            });
-          }
+              
 
         
     }
 
   /* ---*/
  
-  /*---*/
+ 
 
 
 render(){
     
- /* if (localStorage.getItem('user')){
+ if (localStorage.getItem('user')){
     return <Redirect to={'/home'} />
-}*/
+}
     
 return (
-  <Form onSubmit={this.handleLogin}
-     ref={c => {
-        this.form = c;
-     }}>
-
-    <div className="container p-1 mt-5">
+  <form onSubmit={this.handleLogin}>
+    
+    <div class="container pt-3 px-5 mt-5">
       <div class="mt-5">
           <h1>
              <u>Login</u>
           </h1>
              
-          <hr/>
+          <br></br>
   
-       <div class="row px-5">
-          <div class="col-md-7 px-5">
+       <div class="row">
+          <div class="col-lg-7 px-5">
              
             <div class="form-group">
-             <Input type="text" class="form-control" name="username" 
+             <input type="text" class="form-control" name="username" 
                  onChange={this.onChangeUsername}
                  value={this.state.username} placeholder="Email" required="required"/>
             </div>
 
-       
+
             <div class="form-group">
-             <Input type="password" class="form-control" name="password"
+             <input type="password" class="form-control" name="password"
                 value={this.state.password}
                 onChange={this.onChangePassword}
                 placeholder="Password" required="required"/>
             </div>   
+         
           
-            
-          
-           <div class="form-group">
+           <div class="form-group text-center">
               <button type="submit"
                 class="btn btn-primary btn-lg"
                 disabled={this.state.loading}
@@ -146,25 +126,19 @@ return (
               </button>
            </div>
 
-
             {this.state.message && (
-              <div class="form-group">
-                <div class="alert alert-danger" role="alert">
+              <div className="form-group">
+                <div className="alert alert-danger" role="alert">
                   {this.state.message}
                 </div>
               </div>
             )}
-            <CheckButton
-              style={{ display: "none" }}
-              ref={c => {
-                this.checkBtn = c;
-              }}
-            />
+           
   
           </div>
 
-          <div class="col-md-5 ">
-            <img src="./../../login.JPG" alt="login" class=""/>
+          <div class="col-lg-5 px-4">
+            <img src="images/login.jpeg" alt="login" class="ml-5"/>
           </div>
                    
         </div>
@@ -172,47 +146,9 @@ return (
       </div>
   
     </div>
-  </Form>   
+  </form>   
     );
   }
   }
   
   export default withRouter(SignIn);
-
-  /* <div className="form-group">
-              <button type="submit" className="btn btn-primary btn-lg">Login</button>
-          </div>*/
-          
-/*  submitHandler = e => {
-    e.preventDefault()
-    
-    const data ={
-        Email: this.email,
-        Password: this.password
-    }
-  axios
-  .post("http://localhost:5000/api/Accounts/Login", data)
-  .then(response => {
-      
-      localStorage.setItem('user', response.data)
-      this.setState({
-          loggedIn: true
-      })
-      this.props.setUser(response.data)
-
-      *//*
-      if(response.status == 200){
-          alert("Loggin Succesful!")
-      }
-      *//*
-  })
-  .catch(error => {
-      
-      localStorage.setItem('error', error.data)
-      this.setState({
-          loggedIn: false
-      })
-      //this.setState({errorMessage: 'Check your email and password!(' +error.message+ ')'})
-      
-      
-  })}*/
